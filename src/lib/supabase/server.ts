66886ddcd97b34c63c2fd0f54146471c1ee54378
Seungho Han (Hawke) <danhahn61@gmail.com>
2025-04-25 22:53:55 +0900

    fix: login

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,22 +1,21 @@
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
 import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
 
-export async function createClient(response?: NextResponse) {
+export async function createClient() {
   const cookieStore = await cookies()
+
   return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
     cookies: {
-      get(name: string) {
-        return cookieStore.get(name)?.value
+      getAll() {
+        return cookieStore.getAll()
       },
-      set(name: string, value: string, options) {
-        if (response) {
-          response.cookies.set(name, value, options)
-        }
-      },
-      remove(name: string, options) {
-        if (response) {
-          response.cookies.set(name, '', options)
+      setAll(cookiesToSet) {
+        try {
+          cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
+        } catch {
+          // The `setAll` method was called from a Server Component.
+          // This can be ignored if you have middleware refreshing
+          // user sessions.
         }
       },
     },
